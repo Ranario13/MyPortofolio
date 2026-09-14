bf509270097aff744a1288e93d6889fba8c5b081
Ranario13 <ranu.ario@ui.ac.id>
2026-09-14 16:10:43 +0700

    feat: register Experience, Education, and Skill models in Django admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Experience, Education, Skill
 
 # Register your models here.
+
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Skill)
